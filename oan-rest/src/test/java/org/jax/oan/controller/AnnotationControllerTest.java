ff4a7c71e985a2726a8e723ccbd6410fcb32eb96
iimpulse <Michael.Gargano@jax.org>
2023-11-14 14:19:19 -0700

    moving from gradle to maven

--- oan-rest/src/test/java/org/jax/oan/controller/AnnotationControllerTest.java
+++ oan-rest/src/test/java/org/jax/oan/controller/AnnotationControllerTest.java
@@ -20,7 +20,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
-import java.util.List;
 import java.util.Map;
 
 import static org.jax.oan.TestData.*;
