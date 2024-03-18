606fbdcb8006e496054ad30c7dcbe3c7255261f8
iimpulse <Michael.Gargano@jax.org>
2024-03-18 12:01:12 -0600

    adding management endpoint, updating openapi spec 3 with servers and micronaut with a context path instead of custom impl, configuration changes as necessary

--- oan-rest/src/main/java/org/jax/oan/controller/AnnotationController.java
+++ oan-rest/src/main/java/org/jax/oan/controller/AnnotationController.java
@@ -17,7 +17,7 @@ import org.jax.oan.service.*;
 import org.monarchinitiative.phenol.base.PhenolRuntimeException;
 import org.monarchinitiative.phenol.ontology.data.TermId;
 
-@Controller("${api-prefix}/annotation")
+@Controller("/annotation")
 @SerdeImport(Disease.class)
 @SerdeImport(Gene.class)
 @SerdeImport(Phenotype.class)
--- oan-rest/src/main/java/org/jax/oan/controller/SearchController.java
+++ oan-rest/src/main/java/org/jax/oan/controller/SearchController.java
@@ -10,7 +10,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import org.jax.oan.core.SearchDto;
 import org.jax.oan.service.SearchService;
 
-@Controller("${api-prefix}/search")
+@Controller("/search")
 @SerdeImport(SearchDto.class)
 public class SearchController {
 	private SearchService searchService;
