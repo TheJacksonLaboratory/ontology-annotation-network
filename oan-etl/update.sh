79d2538726282061deb681006177676e975c23e4
Michael Gargano <garganomx@gmail.com>
2024-12-12 10:29:53 -0700

    Update update.sh

--- oan-etl/update.sh
+++ oan-etl/update.sh
@@ -15,10 +15,10 @@ wget -P $1 --no-use-server-timestamps http://purl.obolibrary.org/obo/mondo/mondo
 wget -P $1 --no-use-server-timestamps https://raw.githubusercontent.com/monarch-initiative/MAxO/master/maxo.json
 
 # ORPHANET 2 GENE
-wget -P $1 --no-use-server-timestamps  http://www.orphadata.org/data/xml/en_product6.xml
+wget -P $1 --no-use-server-timestamps http://www.orphadata.org/data/xml/en_product6.xml
 
 # GENE INFO
-wget -P $1 --no-use-server-timestamps  https://g-a8b222.dd271.03c0.data.globus.org/pub/databases/genenames/hgnc/tsv/hgnc_complete_set.txt
+wget -P $1 --no-use-server-timestamps https://storage.googleapis.com/public-download-files/hgnc/tsv/tsv/hgnc_complete_set.txt
 
 # PHENOTYPE HPOA
 wget https://github.com/obophenotype/human-phenotype-ontology/releases/latest/download/phenotype.hpoa -O $1phenotype.hpoa
